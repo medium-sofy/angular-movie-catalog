7442fcbd986d8b361ad3b4a0589f7e6e478673ee
Mohamed Sofy <mediumsofy@gmail.com>
2025-04-07 10:04:43 +0200

    Add movie details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'; // <-- Import
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // <-- Add default route
+  { path: 'movie/:id', component: MovieDetailsComponent },
   // other routes...
   { path: '**', redirectTo: '' } // Optional: Redirect unknown paths to home
 ];
