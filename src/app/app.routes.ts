a3620def4a5f6229e1f374e78f7ca49fd1d8b187
Mohamed Sofy <mediumsofy@gmail.com>
2025-04-07 10:51:47 +0200

    Add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'; // <-- Import
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // <-- Add default route
   { path: 'movie/:id', component: MovieDetailsComponent },
   // other routes...
-  { path: '**', redirectTo: '' } // Optional: Redirect unknown paths to home
+  { path: '**', component: PageNotFoundComponent } // Optional: Redirect unknown paths to home
 ];
 
 @NgModule({
