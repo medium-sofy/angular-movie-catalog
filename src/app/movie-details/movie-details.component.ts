3602f77a7850d82a9f4c0c88fc5555f585719eda
Mohamed Sofy <mediumsofy@gmail.com>
2025-04-07 15:49:02 +0200

    Inject movie service into movie details component

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './movie-details.component.css'
 })
 export class MovieDetailsComponent {
+private movieId!: number;
+private movie!: any;
 
+constructor(private movieService: MovieService, private route: ActivatedRoute) {}
+
+ngOnInit(): void {
+  this.movieId = Number(this.route.snapshot.paramMap.get('id'));
+  this.movieService.getMovieDetails(this.movieId).subscribe((movie : any) => {
+    this.movie = movie;
+    console.log(this.movie);
+  });
+}
 }
