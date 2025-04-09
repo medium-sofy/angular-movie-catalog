ae29d9b1e610e54726bd51632d03d7d9e5d8f896
Mohamed Sofy <mediumsofy@gmail.com>
2025-04-09 02:26:35 +0200

    Display single movie details dynamically from movie service

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,33 +1,94 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-movie-details',
-  imports: [],
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.css'
+  styleUrls: ['./movie-details.component.css'],
+  // Include the necessary pipes as providers
+  providers: [DatePipe, DecimalPipe, RouterLink]
 })
-export class MovieDetailsComponent {
-   movieId!: number;
-   movie!: any;
+export class MovieDetailsComponent implements OnInit {
+  movie: any;
+  loading = true;
+  error = '';
 
-  constructor(private movieService: MovieService, private route: ActivatedRoute) { }
-  getPosterUrl(path:string): string {
-    return `https://image.tmdb.org/t/p/w500/${path}`;
-  }
-  ngOnInit() {
-    this.movieId = Number(this.route.snapshot.paramMap.get('id'));
-    this.movieService.getMovieDetails(this.movieId).subscribe((movie: any) => {
-      this.movie = movie;
-      console.log(this.movie);
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const movieId = Number(params.get('id'));
+      if (movieId) {
+        this.getMovieDetails(movieId);
+      }
     });
   }
+
+  getMovieDetails(id: number): void {
+    this.loading = true;
+    this.error = '';
+    
+    this.movieService.getMovieDetails(id).subscribe({
+      next: (data) => {
+        this.movie = data;
+        this.loading = false;
+        console.log(this.movie)
+      },
+      error: (err) => {
+        this.error = 'Failed to load movie details: ' + err.message;
+        this.loading = false;
+      }
+    });
+  }
+
+  getPosterUrl(posterPath: string): string {
+    return `https://image.tmdb.org/t/p/w500${posterPath}`;
+  }
   
+  getCompanyLogoUrl(logoPath: string): string {
+    return `https://image.tmdb.org/t/p/w200${logoPath}`;
+  }
+
+  handleImageError(event: any): void {
+    event.target.src = 'assets/images/placeholder.png';
+  }
+
+  formatRuntime(minutes: number): string {
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+    return `${hours}h ${mins}m`;
+  }
+
   getLanguagesList(languages: any[]): string {
     if (!languages || languages.length === 0) {
-      return '';
+      return 'Information not available';
     }
     return languages.map(lang => lang.english_name || lang.name).join(', ');
+  }
+
+  toggleWatchlist(movie: any): void {
+    movie.inWatchlist = !movie.inWatchlist;
+    // Implement your watchlist logic here
+  }
+  
+  /**
+   * Gets the initials from a company name for use in the placeholder
+   */
+  getCompanyInitials(name: string): string {
+    if (!name) return '?';
+    
+    // For names with multiple words, get first letter of each word (up to 2)
+    const words = name.split(' ');
+    if (words.length > 1) {
+      return (words[0][0] + words[1][0]).toUpperCase();
+    }
+    
+    // For single word names, return first 2 letters
+    return name.substring(0, 2).toUpperCase();
   }
 }
